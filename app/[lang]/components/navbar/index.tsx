fdb5c4f6eaf3b7e9c33595dd95429d629d3293e0
Ashley <ashley.liz2605@gmail.com>
2024-07-18 19:49:18 -0600

    Navbar now hides when scrolling down

--- app/[lang]/components/navbar/index.tsx
+++ app/[lang]/components/navbar/index.tsx
@@ -1,10 +1,30 @@
+'use client';
 import MobileNavigationMenu from './mobileNavMenu';
 import NavigationLinks from './navLinks';
 import LanguagePicker from './languagePicker';
+import { useEffect, useState } from 'react';
 
 export default function Navbar({ lang }: { lang: string }) {
+    const [isShowing, setIsShowing] = useState(true);
+    const [prevPosition, setPrevPosition] = useState(0);
+    useEffect(() => {
+        function onScroll() {
+            const currentPosition = window.scrollY;
+
+            if (prevPosition > currentPosition) setIsShowing(true);
+            else setIsShowing(false);
+
+            setPrevPosition(currentPosition);
+        }
+        document.addEventListener('scroll', onScroll);
+
+        return () => {
+            document.removeEventListener('scroll', onScroll);
+        };
+    });
     return (
-        <nav className="fixed z-20 w-full bg-white px-5 py-3 text-black shadow md:px-10 min-[840px]:px-20 lg:px-36">
+        <nav
+            className={`fixed z-20 w-full bg-white px-5 py-3 text-black shadow transition md:px-10 min-[840px]:px-20 lg:px-36 ${isShowing ? '-translate-y-0' : '-translate-y-full'}`}>
             <div className="flex w-full items-center justify-between">
                 <MobileNavigationMenu lang={lang} />
                 <a className="text-xl text-primary" href={`/${lang}`}>
