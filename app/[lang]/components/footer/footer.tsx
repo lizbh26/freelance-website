8d52959b05d7573e857e3442649d444cee0c7143
Ashley <ashley.liz2605@gmail.com>
2024-07-19 19:02:50 -0600

    Replaced my name with Digital Obelisk in footer, added social media placeholder

--- app/[lang]/components/footer/footer.tsx
+++ app/[lang]/components/footer/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
-        <footer className="mt-5 flex items-center justify-center bg-neutral-100 py-3">
-            <p>Ashley Lizbeth &copy;2024</p>
+        <footer className="mt-5 flex flex-col items-center justify-center bg-neutral-100 py-3">
+            <p>Digital Obelisk &copy;2024</p>
+            <p>[social media goes here]</p>
         </footer>
     );
 }
