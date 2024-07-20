1969b2bd346f3cc61f0c727eff33d3e838099b9b
Ashley <ashley.liz2605@gmail.com>
2024-07-19 19:01:22 -0600

    Added informative text to contact page

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -12,48 +12,55 @@ export default function ContactUs({
                     Contact us
                 </h1>
             </header>
-            <div className="flex w-full items-center justify-center py-6">
-                <form
-                    action=""
-                    className="w-full px-5 sm:w-3/4 md:w-2/3 md:px-0 lg:w-1/2">
-                    <div className="my-3 flex flex-col">
-                        <label htmlFor="email">Your email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="rounded border-2 border-black px-2"
-                        />
-                    </div>
-                    <div className="my-3 flex flex-col">
-                        <label htmlFor="name">Your name / company</label>
-                        <input
-                            type="text"
-                            id="name"
-                            className="rounded border-2 border-black px-2"
-                        />
-                    </div>
-                    <div className="my-3 flex flex-col">
-                        <label htmlFor="description">
-                            Briefly describe your project
-                        </label>
-                        <textarea
-                            rows={5}
-                            id="description"
-                            className="rounded border-2 border-black px-2"
-                        />
-                    </div>
-                    <div className="flex w-full items-center justify-center">
-                        <button className="flex items-center justify-center gap-2 rounded border-4 border-primary bg-primary px-5 py-2 font-bold text-white transition hover:scale-105 hover:bg-primary hover:text-white md:bg-white md:text-black">
-                            Send{' '}
-                            <span
-                                className="inline-block"
-                                dangerouslySetInnerHTML={{
-                                    __html: feather.icons.send.toSvg(),
-                                }}></span>
-                        </button>
-                    </div>
-                </form>
-            </div>
+            <article className="px-5 py-6 text-center">
+                <h3 className="text-xl">
+                    Please fill out this form and we'll get in contact as soon
+                    as possible.
+                </h3>
+                <section className="mb-10 flex w-full items-center justify-center">
+                    <form
+                        action=""
+                        className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2">
+                        <div className="my-3 flex flex-col">
+                            <label htmlFor="email">Your email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="rounded border-2 border-black px-2"
+                            />
+                        </div>
+                        <div className="my-3 flex flex-col">
+                            <label htmlFor="name">Your name / company</label>
+                            <input
+                                type="text"
+                                id="name"
+                                className="rounded border-2 border-black px-2"
+                            />
+                        </div>
+                        <div className="my-3 flex flex-col">
+                            <label htmlFor="description">
+                                Briefly describe your project
+                            </label>
+                            <textarea
+                                rows={5}
+                                id="description"
+                                className="rounded border-2 border-black px-2"
+                            />
+                        </div>
+                        <div className="flex w-full items-center justify-center">
+                            <button className="flex items-center justify-center gap-2 rounded border-4 border-primary bg-primary px-5 py-2 font-bold text-white transition hover:scale-105 hover:bg-primary hover:text-white md:bg-white md:text-black">
+                                Send{' '}
+                                <span
+                                    className="inline-block"
+                                    dangerouslySetInnerHTML={{
+                                        __html: feather.icons.send.toSvg(),
+                                    }}></span>
+                            </button>
+                        </div>
+                    </form>
+                </section>
+                <p>For more enquiries, you can contact [email here]</p>
+            </article>
         </main>
     );
 }
