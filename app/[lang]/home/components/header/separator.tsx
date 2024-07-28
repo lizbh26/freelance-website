efa07b72d9ebf511364941b49115c8502b8524f9
Ashley <ashley.liz2605@gmail.com>
2024-07-27 19:09:29 -0600

    getWindowDimensions was unreliable, changed header separator to load both images at once and only show one at a time

--- app/[lang]/home/components/header/separator.tsx
+++ app/[lang]/home/components/header/separator.tsx
@@ -1,16 +1,23 @@
 'use client';
-import useWindowDimensions from '@/app/hooks/useWindowDimensions';
 import Image from 'next/image';
 
 export default function HeaderSeparator() {
-    const { width } = useWindowDimensions();
     return (
-        <Image
-            className="w-full overflow-hidden"
-            src={`/assets/header-separator${width < 800 ? '-mobile' : ''}.png`}
-            alt=""
-            width={width < 800 ? 800 : 1980}
-            height={'200'}
-        />
+        <>
+            <Image
+                className="w-full overflow-hidden min-[800px]:hidden"
+                src="/assets/header-separator-mobile.png"
+                alt=""
+                width={800}
+                height={200}
+            />
+            <Image
+                className="w-full overflow-hidden max-[800px]:hidden"
+                src={`/assets/header-separator.png`}
+                alt=""
+                width={1980}
+                height={200}
+            />
+        </>
     );
 }
