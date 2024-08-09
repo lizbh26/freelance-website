409e67640dd545bbbcc170731082e7a946dd4236
Ashley <ashley.liz2605@gmail.com>
2024-08-09 16:50:57 -0600

    Removed pricing link

--- app/[lang]/components/navbar/navLinks.tsx
+++ app/[lang]/components/navbar/navLinks.tsx
@@ -28,11 +28,6 @@ export default function NavigationLinks({ lang }: { lang: string }) {
                 Testimonials
             </a>
             <a
-                className={`p-2 transition hover:border-b-primary md:border-b-4 md:border-b-white md:px-0 ${page === '/pricing' ? 'border-b-4 border-b-primary' : ''}`}
-                href={`/${lang}/pricing`}>
-                Pricing
-            </a>
-            <a
                 className="rounded bg-primary px-3 py-2 text-center font-bold text-white transition hover:scale-105 md:mx-3"
                 href={`/${lang}/contact`}>
                 Schedule a{' '}
