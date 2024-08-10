cc37b1c8204af565a477ed36e34c4b741fac117d
Ashley <ashley.liz2605@gmail.com>
2024-08-09 19:07:34 -0600

    Centered header on mobile screens

--- app/[lang]/home/components/header/index.tsx
+++ app/[lang]/home/components/header/index.tsx
@@ -8,11 +8,11 @@ export default async function Header({ lang }: { lang: string }) {
     return (
         <header id="top" className="">
             <div className="flex justify-center bg-secondary pb-12 pt-36 md:justify-start md:px-20 md:pb-6 lg:px-36">
-                <div className="flex w-5/6 flex-col items-start md:w-1/2">
+                <div className="flex w-5/6 flex-col items-center text-center md:w-1/2 md:items-start md:text-start">
                     <h1 className="mb-4 text-4xl font-bold">
                         {t('header.title')}
                     </h1>
-                    <p className="text-start">{t('header.subtitle')}</p>
+                    <p>{t('header.subtitle')}</p>
                     <a
                         href={`/${lang}/contact`}
                         className="mt-8 inline-block rounded bg-primary px-5 py-2 text-xl font-bold text-white transition hover:scale-105">
