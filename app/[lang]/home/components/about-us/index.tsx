bb7e35f5df893e9adcbe3a9e7ed387cc9c03443b
Ashley <ashley.liz2605@gmail.com>
2024-08-09 19:09:17 -0600

    Added widths to about us section in preparation for image at left side

--- app/[lang]/home/components/about-us/index.tsx
+++ app/[lang]/home/components/about-us/index.tsx
@@ -2,30 +2,27 @@ export default function AboutUs() {
     return (
         <section
             id="about-us"
-            className="lg:px-30 scroll-mt-20 px-5 py-10 md:px-16">
-            <div className="flex flex-col items-center justify-center gap-6 md:items-end md:text-end">
-                <h2 className="text-center text-3xl font-bold md:w-2/3">
-                    Work with people who care about your clients like you do
-                </h2>
-                <span className="flex flex-col gap-2">
-                    <p>
-                        We understand that the best and worst user experiences
-                        almost always come from technology: for every online
-                        order that gets delivered without flaw there is a
-                        website that is simply infuriating to use, and so people{' '}
-                        {'don’t'} bother.
-                    </p>
-                    <p>
-                        For this reason, our focus is always on the final client
-                        and on how they will perceive our solution, the product
-                        and your brand.
-                    </p>
-                </span>
-                <a
-                    className="w-fit rounded bg-primary px-4 py-2 font-bold text-white shadow transition hover:scale-105"
-                    href="/about">
-                    Learn more
-                </a>
+            className="lg:px-30 scroll-mt-20 px-5 py-10 sm:px-16 md:px-20">
+            <div className="md:flex md:flex-row-reverse">
+                <div className="flex flex-col items-center justify-center gap-6 md:w-3/5 md:items-end md:text-end xl:w-1/2">
+                    <h2 className="text-center text-3xl font-bold md:text-end">
+                        Work with people who care about your clients like you do
+                    </h2>
+                    <span className="flex flex-col gap-2">
+                        <p>
+                            We understand that the best and worst user
+                            experiences almost always come from technology: for
+                            every online order that gets delivered without flaw
+                            there is a website that is simply infuriating to
+                            use, and so people {'don’t'} bother.
+                        </p>
+                        <p>
+                            For this reason, our focus is always on the final
+                            client and on how they will perceive our solution,
+                            the product and your brand.
+                        </p>
+                    </span>
+                </div>
             </div>
         </section>
     );
