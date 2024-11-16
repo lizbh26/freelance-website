1e03996658d49afd661429170b95770ee42fd603
Ashley <ashley.liz2605@gmail.com>
2024-11-16 09:05:31 -0600

    Header separator now changes at 700px

--- app/[lang]/home/components/header/separator.tsx
+++ app/[lang]/home/components/header/separator.tsx
@@ -1,18 +1,17 @@
-'use client';
 import Image from 'next/image';
 
 export default function HeaderSeparator() {
     return (
         <>
             <Image
-                className="w-full overflow-hidden min-[800px]:hidden"
+                className="w-full overflow-hidden min-[700px]:hidden"
                 src="/assets/header-separator-mobile.png"
                 alt=""
                 width={800}
                 height={200}
             />
             <Image
-                className="w-full overflow-hidden max-[800px]:hidden"
+                className="w-full overflow-hidden max-[700px]:hidden"
                 src={`/assets/header-separator.png`}
                 alt=""
                 width={1980}
