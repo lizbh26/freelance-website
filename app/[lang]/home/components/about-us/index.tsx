81537045657c98a99fa40b4cb609b2dd0bb873c0
Ashley <ashley.liz2605@gmail.com>
2024-11-15 22:50:40 -0600

    Added image to home's about us

--- app/[lang]/home/components/about-us/index.tsx
+++ app/[lang]/home/components/about-us/index.tsx
@@ -2,10 +2,10 @@ export default function AboutUs() {
     return (
         <section
             id="about-us"
-            className="lg:px-30 scroll-mt-20 px-5 py-10 sm:px-16 md:px-20">
-            <div className="md:flex md:flex-row-reverse">
-                <div className="flex flex-col items-center justify-center gap-6 md:w-3/5 md:items-end md:text-end xl:w-1/2">
-                    <h2 className="text-center text-3xl font-bold md:text-end">
+            className="scroll-mt-20 px-5 pb-20 pt-10 sm:px-16 md:px-28 md:pt-0 lg:px-32">
+            <div className="lg:flex lg:flex-row-reverse">
+                <div className="flex flex-col items-center justify-center gap-6 lg:w-3/5 lg:items-end lg:text-end xl:w-1/2">
+                    <h2 className="text-center text-3xl font-bold lg:text-end">
                         Work with people who care about your clients like you do
                     </h2>
                     <span className="flex flex-col gap-2">
@@ -22,6 +22,15 @@ export default function AboutUs() {
                             the product and your brand.
                         </p>
                     </span>
+                </div>
+                <div className="mt-8 flex items-center justify-center px-4 lg:w-2/5 xl:w-1/2">
+                    <img
+                        className="object-cover"
+                        width={400}
+                        height={300}
+                        src="/assets/img/user-experience.png"
+                        alt="Good user experience"
+                    />
                 </div>
             </div>
         </section>
