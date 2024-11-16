fe70800a0eafbe883f8bd9c9a58dd0e234870241
Ashley <ashley.liz2605@gmail.com>
2024-11-16 08:57:27 -0600

    Quick solution to translate title of language picker

--- app/[lang]/components/navbar/languagePicker.tsx
+++ app/[lang]/components/navbar/languagePicker.tsx
@@ -44,8 +44,8 @@ export default function LanguagePicker({ lang }: { lang: string }) {
             </button>
             <div
                 className={`absolute right-0 mt-6 w-1/2 min-w-44 overflow-hidden bg-white shadow transition-all duration-300 sm:w-1/3 md:-left-12 md:right-auto md:w-auto md:min-w-36 lg:mt-4 ${isOpen ? 'translate-x-0 md:max-h-screen' : 'translate-x-full md:max-h-0 md:translate-x-0'}`}>
-                <h3 className="py-2 text-center text-xl text-primary">
-                    Choose language
+                <h3 className="p-2 text-center text-xl text-primary">
+                    {lang == 'en' ? 'Choose language' : 'Cambiar idioma'}
                 </h3>
                 <hr />
                 <ul className="flex flex-col gap-2 px-5 py-2 md:px-2">
