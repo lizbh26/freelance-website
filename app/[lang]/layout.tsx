f935290c0ddebe6b54a6e5d70ced4f590ada526c
Ashley <ashley.liz2605@gmail.com>
2024-11-16 08:58:30 -0600

    Linked navbar translations to navbar client component via props

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 
 import Navbar from './components/navbar';
 import Footer from './components/footer/footer';
+import { getTranslations } from '../i18n';
 
 export async function generateStaticParams() {
     return languages.map((lang) => ({ lang }));
@@ -12,17 +13,28 @@ export async function generateStaticParams() {
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
     params: { lang },
 }: Readonly<{
     children: React.ReactNode;
     params: { lang: string };
 }>) {
+    const { t: tNav } = await getTranslations(lang, 'navbar');
+
     return (
         <html lang={lang} className="scroll-smooth">
             <body className={inter.className + ' bg-neutral-100'}>
-                <Navbar lang={lang} />
+                <Navbar
+                    lang={lang}
+                    links={{
+                        home: tNav('home'),
+                        about: tNav('about'),
+                        services: tNav('services'),
+                        cta_1: tNav('cta-1'),
+                        cta_2: tNav('cta-2'),
+                    }}
+                />
                 <div className="bg-white">{children}</div>
                 <Footer />
             </body>
