c8f0c6860cbc8aa3d2460a78241b1eecce2ade12
Ashley <ashley.liz2605@gmail.com>
2024-11-15 22:45:38 -0600

    Changed paddings, removed /testimonials link a page is not ready

--- app/[lang]/components/navbar/navLinks.tsx
+++ app/[lang]/components/navbar/navLinks.tsx
@@ -8,31 +8,29 @@ export default function NavigationLinks({ lang }: { lang: string }) {
     return (
         <>
             <a
-                className={`p-2 transition md:hidden ${page === '' ? 'border-b-4 border-b-primary' : ''}`}
+                className={`my-1 w-fit border-b-4 px-2 py-1 transition hover:border-b-primary md:my-0 md:hidden md:px-0 md:py-2 ${page === '' ? 'border-b-primary' : 'border-b-white'}`}
                 href={`/${lang}`}>
                 Home
             </a>
             <a
-                className={`p-2 transition hover:border-b-primary md:border-b-4 md:border-b-white md:px-0 ${page === '/about' ? 'border-b-4 border-b-primary' : ''}`}
+                className={`my-1 w-fit border-b-4 px-2 py-1 transition hover:border-b-primary md:my-0 md:px-0 md:py-2 ${page === '/about' ? 'border-b-primary' : 'border-b-white'}`}
                 href={`/${lang}/about`}>
                 About us
             </a>
             <a
-                className={`p-2 transition hover:border-b-primary md:border-b-4 md:border-b-white md:px-0 ${page === '/services' ? 'border-b-4 border-b-primary' : ''}`}
+                className={`my-1 w-fit border-b-4 px-2 py-1 transition hover:border-b-primary md:my-0 md:px-0 md:py-2 ${page === '/services' ? 'border-b-primary' : 'border-b-white'}`}
                 href={`/${lang}/services`}>
                 Services
             </a>
-            <a
-                className={`p-2 transition hover:border-b-primary md:border-b-4 md:border-b-white md:px-0 ${page === '/testimonials' ? 'border-b-4 border-b-primary' : ''}`}
+            {/* <a
+                className={`my-1 w-fit border-b-4 px-2 py-1 transition hover:border-b-primary md:my-0 md:px-0 md:py-2 ${page === '/testimonials' ? 'border-b-primary' : 'border-b-white'}`}
                 href={`/${lang}/testimonials`}>
                 Testimonials
-            </a>
+            </a> */}
             <a
                 className="rounded bg-primary px-3 py-2 text-center font-bold text-white transition hover:scale-105 md:mx-3"
                 href={`/${lang}/contact`}>
-                Schedule a{' '}
-                <br className="min-[410px]:hidden md:block lg:hidden" /> free
-                call
+                Schedule a <br className="min-[410px]:hidden" /> free call
             </a>
         </>
     );
