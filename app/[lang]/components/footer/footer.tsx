e6c550858e64ea1140b49ae3c449dfe0fbea11f0
Ashley <ashley.liz2605@gmail.com>
2024-12-06 20:08:31 -0600

    Cleaned up footer

--- app/[lang]/components/footer/footer.tsx
+++ app/[lang]/components/footer/footer.tsx
@@ -1,8 +1,7 @@
 export default function Footer() {
     return (
         <footer className="mt-5 flex flex-col items-center justify-center bg-neutral-100 py-3">
-            <p>Digital Obelisk &copy;2024</p>
-            <p>[social media goes here]</p>
+            <p>Digital Obelisk @2024</p>
         </footer>
     );
 }
