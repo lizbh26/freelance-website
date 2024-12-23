66eb1ddc68678dc56bfad24a1e2434e884ed8cc8
Ashley <ashley.liz2605@gmail.com>
2024-12-22 19:13:46 -0600

    Service list is now bordered, service chooser dropdown now displays the correct text

--- app/[lang]/services/components/service_list.tsx
+++ app/[lang]/services/components/service_list.tsx
@@ -11,44 +11,46 @@ export default function ServiceList({ services }: { services: Service[] }) {
 
     const selectedName = services.find(
         (service) => service.id === selectedID,
-    )?.title;
+    )?.option;
 
     if (!selectedID || !selectedName)
         redirect(`${pathname}?selected=${ServicesIDs[0]}`);
 
     return (
-        <div className="flex flex-col items-center justify-center md:flex-row-reverse md:items-start md:justify-start">
-            <ServiceChooser
-                options={services.map((service) => {
-                    return {
-                        id: service.id,
-                        name: service.title,
-                    } as ServiceOption;
-                })}
-                selected={{ id: selectedID, name: selectedName }}
-            />
-            <div className="md:w-2/3 md:pr-4">
-                {services.map((service) => (
-                    <section
-                        id={service.id}
-                        key={service.id}
-                        className={` ${selectedID !== service.id ? 'hidden' : ''}`}>
-                        <h2 className="text-center text-3xl font-bold">
-                            {service.title}
-                        </h2>
-                        <h3
-                            className="mb-4 text-center text-lg italic text-neutral-500"
-                            dangerouslySetInnerHTML={{
-                                __html: service.tagline,
-                            }}></h3>
-                        <div
-                            className="flex flex-col gap-2 text-center"
-                            dangerouslySetInnerHTML={{
-                                __html: service.body,
-                            }}></div>
-                    </section>
-                ))}
+        <section className="rounded-md border-2 border-gray-100 px-10 py-8 shadow-md md:py-16">
+            <div className="flex flex-col items-center justify-center md:flex-row-reverse md:justify-start">
+                <ServiceChooser
+                    options={services.map((service) => {
+                        return {
+                            id: service.id,
+                            name: service.option,
+                        } as ServiceOption;
+                    })}
+                    selected={{ id: selectedID, name: selectedName }}
+                />
+                <div className="md:w-2/3 md:pr-4 lg:w-3/4">
+                    {services.map((service) => (
+                        <section
+                            id={service.id}
+                            key={service.id}
+                            className={` ${selectedID !== service.id ? 'hidden' : ''}`}>
+                            <h2 className="text-center text-3xl font-bold">
+                                {service.title}
+                            </h2>
+                            <h3
+                                className="mb-4 text-center text-lg italic text-neutral-500"
+                                dangerouslySetInnerHTML={{
+                                    __html: service.tagline,
+                                }}></h3>
+                            <div
+                                className="flex flex-col gap-2 text-justify"
+                                dangerouslySetInnerHTML={{
+                                    __html: service.body,
+                                }}></div>
+                        </section>
+                    ))}
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
